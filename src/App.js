80aa2f163ef92d19f982f18959b7545149e32dfa
natagr23 <nata_garcia23@hotmail.com>
2022-03-27 17:59:16 -0500

    Setting search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import * as React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import { StyledEngineProvider } from '@mui/material/styles';
+import SearchBar from './components/SearchBar/SearchBar';
 
-export function App() {
+function App() {
   return (
     <div className="App">
       <Navbar />
+      <StyledEngineProvider injectFirst>
+        <SearchBar />
+      </StyledEngineProvider>
+      ,
     </div>
   );
 }
