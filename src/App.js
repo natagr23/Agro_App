b62b8d4ae5767dd4b799ac571367e498dcc5df1e
natagr23 <nata_garcia23@hotmail.com>
2022-03-28 19:14:47 -0500

    setting api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { StyledEngineProvider } from '@mui/material/styles';
 import SearchBar from './components/SearchBar/SearchBar';
+import Api from './components/Api/Api';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <StyledEngineProvider injectFirst>
+        <Api />
         <SearchBar />
       </StyledEngineProvider>
       ,
---
+++ src/components/Api/Api.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Api() {
+  const [posts, setPosts] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    try {
+      const response = await axios(
+        'https://react-course-http-40bac-default-rtdb.firebaseio.com/movies.json'
+      );
+      console.log(response);
+      setMovies(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
+
+  //   const loadedMovies = [];
+
+  //   for (const key in data) {
+  //     loadedMovies.push({
+  //       id: key,
+  //       title: data[key].title,
+  //       openingText: data[key].openingText,
+  //       releaseDate: data[key].releaseDate,
+  //     });
+  //   }
+
+  return (
+    <div>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <h3> Random joke generator😂 </h3>
+      <p>{movies.title}</p>
+      <button onClick={fetchMoviesHandler}>Get new joke</button>
+    </div>
+  );
+}
