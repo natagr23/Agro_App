21a0be7c801f7bcabde11dd51934352bcf23fadf
natagr23 <nata_garcia23@hotmail.com>
2022-03-31 18:49:33 -0500

    setting SearchBar with mui

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import * as React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { StyledEngineProvider } from '@mui/material/styles';
 import SearchBar from './components/SearchBar/SearchBar';
-import Api from './components/Api/Api';
+// import Api from './components/Api/Api';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <SearchBar /> */}
+      <SearchBar />
       <StyledEngineProvider injectFirst></StyledEngineProvider>
-      <Api />
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,110 +1,24 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import TextField from '@mui/material/TextField';
-import { DataGrid } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
-import ClearIcon from '@mui/icons-material/Clear';
-import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
-function escapeRegExp(value) {
-  return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-}
+import Api from '../Api/Api';
 
-function QuickSearchToolbar(props) {
+export default function SearchBar(props) {
   return (
-    <Box
-      sx={{
-        p: 10,
-        pb: 0,
-      }}
-    >
-      <TextField
-        variant="standard"
-        value={props.value}
-        onChange={props.onChange}
-        placeholder="Search…"
-        InputProps={{
-          startAdornment: <SearchIcon fontSize="small" />,
-          endAdornment: (
-            <IconButton
-              title="Clear"
-              aria-label="Clear"
-              size="small"
-              style={{ visibility: props.value ? 'visible' : 'hidden' }}
-              onClick={props.clearSearch}
-            >
-              <ClearIcon fontSize="small" />
-            </IconButton>
-          ),
-        }}
+    <>
+      <Box
         sx={{
-          width: {
-            xs: 1,
-            sm: 'auto',
-          },
-          m: (theme) => theme.spacing(1, 0.5, 1.5),
-          '& .MuiSvgIcon-root': {
-            mr: 0.5,
-          },
-          '& .MuiInput-underline:before': {
-            borderBottom: 1,
-            borderColor: 'divider',
-          },
+          '& > :not(style)': { m: 5, width: '30ch' },
+          // width: '100%',
+          // maxWidth: 0,
+          // bgcolor: 'background.paper',
         }}
-      />
-    </Box>
-  );
-}
-
-QuickSearchToolbar.propTypes = {
-  clearSearch: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
-};
-
-const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
-
-export default function QuickFilteringGrid() {
-  const { data } = useDemoData({
-    dataSet: 'Employee',
-    visibleFields: VISIBLE_FIELDS,
-    rowLength: 100,
-  });
-
-  const [searchText, setSearchText] = React.useState('');
-  const [rows, setRows] = React.useState(data.rows);
-
-  const requestSearch = (searchValue) => {
-    setSearchText(searchValue);
-    const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
-    const filteredRows = data.rows.filter((row) => {
-      return Object.keys(row).some((field) => {
-        return searchRegex.test(row[field].toString());
-      });
-    });
-    setRows(filteredRows);
-  };
-
-  React.useEffect(() => {
-    setRows(data.rows);
-  }, [data.rows]);
-
-  return (
-    <Box sx={{ height: 400, width: 1 }}>
-      <DataGrid
-        components={{ Toolbar: QuickSearchToolbar }}
-        rows={rows}
-        columns={data.columns}
-        componentsProps={{
-          toolbar: {
-            value: searchText,
-            onChange: (event) => requestSearch(event.target.value),
-            clearSearch: () => requestSearch(''),
-          },
-        }}
-      />
-    </Box>
+        autoComplete="on"
+      >
+        <Api />
+      </Box>
+    </>
   );
 }
