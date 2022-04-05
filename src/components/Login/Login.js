91eb1d01115bbbe8e266e86cef5e20a78327d1a9
natagr23 <nata_garcia23@hotmail.com>
2022-04-05 11:33:27 -0500

    Create text fields and Button for Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,54 +1,55 @@
 import * as React from 'react';
-import Input from '@mui/material/Input';
-import Button from '@mui/material/Button';
-import FormControl from '@mui/material/FormControl';
-import Box from '@mui/material/Box';
+import Form from '../../components/common/Form';
 
 export default function Login() {
   return (
     <>
-      <main>
-        <h2>Services</h2>
-        <p>Login....</p>
-
-        <FormControl>
-          <Input
-            // id="my-input"
-            aria-describedby="my-helper-text"
-            // ref={1}
-            id="email"
-            label="E-Mail"
-            type="email"
-            // isValid={}
-            // value={value}
-            // onChange={onChange}
-            // onBlur={}
-          />
-          <Input
-            id="my-input"
-            aria-describedby="my-helper-text"
-            // ref={passwordInputRef}
-            // id="password"
-            label="Password"
-            type="password"
-            // isValid={passwordIsValid}
-            // value={passwordState.value}
-            // onChange={passwordChangeHandler}
-            // onBlur={validatePasswordHandler}
-          />
-
-          <Box
-            sx={{
-              p: 5,
-              pb: 1,
-            }}
-          >
-            <Button type="submit" variant="contained">
-              Login
-            </Button>
-          </Box>
-        </FormControl>
-      </main>
+      <h2>Login</h2>
+      <p>Login....</p>
+      <Form />
     </>
   );
 }
+
+// import Input from '@mui/material/Input';
+// import Button from '@mui/material/Button';
+// import FormControl from '@mui/material/FormControl';
+// import Box from '@mui/material/Box';
+
+// <FormControl>
+//           <Input
+//             // id="my-input"
+//             aria-describedby="my-helper-text"
+//             // ref={1}
+//             id="email"
+//             label="E-Mail"
+//             type="email"
+//             // isValid={}
+//             // value={value}
+//             // onChange={onChange}
+//             // onBlur={}
+//           />
+//           <Input
+//             id="my-input"
+//             aria-describedby="my-helper-text"
+//             // ref={passwordInputRef}
+//             // id="password"
+//             label="Password"
+//             type="password"
+//             // isValid={passwordIsValid}
+//             // value={passwordState.value}
+//             // onChange={passwordChangeHandler}
+//             // onBlur={validatePasswordHandler}
+//           />
+
+//           <Box
+//             sx={{
+//               p: 5,
+//               pb: 1,
+//             }}
+//           >
+//             <Button type="submit" variant="contained">
+//               Login
+//             </Button>
+//           </Box>
+//         </FormControl>
