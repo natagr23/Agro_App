9b928dab3ff40f725d34294c07dac818c047fb41
natagr23 <nata_garcia23@hotmail.com>
2022-04-06 18:20:40 -0500

    setting login and logout component

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ import {
   createUserWithEmailAndPassword,
 } from 'firebase/auth';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -25,26 +28,37 @@ function App() {
   const handleAction = (id) => {
     const authentication = getAuth();
     if (id === 1) {
-      signInWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
           navigate('/components/Home/Home');
           sessionStorage.setItem(
             'Auth Token',
             response._tokenResponse.refreshToken
           );
-        }
-      );
+        })
+        .catch((error) => {
+          if (error.code === 'auth/wrong-password') {
+            toast.error('Please check the Password');
+          }
+          if (error.code === 'auth/user-not-found') {
+            toast.error('Please check the Email');
+          }
+        });
     }
     if (id === 2) {
-      createUserWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
           navigate('/components/Home/Home');
           sessionStorage.setItem(
             'Auth Token',
             response._tokenResponse.refreshToken
           );
-        }
-      );
+        })
+        .catch((error) => {
+          if (error.code === 'auth/email-already-in-use') {
+            toast.error('Email Already in Use');
+          }
+        });
     }
   };
   useEffect(() => {
@@ -56,6 +70,7 @@ function App() {
   }, [navigate]);
   return (
     <React.Fragment>
+      <ToastContainer />
       <StyledEngineProvider injectFirst>
         <Navbar />
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export default function Home() {
+  const handleLogout = () => {
+    sessionStorage.removeItem('Auth Token');
+    navigate('/login');
+  };
   let navigate = useNavigate();
   useEffect(() => {
     let authToken = sessionStorage.getItem('Auth Token');
 
     if (authToken) {
-      navigate('/home');
+      navigate('/components/Home/Home');
     }
 
     if (!authToken) {
@@ -19,6 +24,7 @@ export default function Home() {
     <>
       <div> Home Page</div>
       <p> Home...</p>
+      <Button onClick={handleLogout}>Log out</Button>
       <SearchBar />
     </>
   );
