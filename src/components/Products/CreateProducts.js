573049fc2713e90f34f1f1e7b9e2e00e61513799
natagr23 <nata_garcia23@hotmail.com>
2022-06-01 18:01:03 -0500

    adding Mui Table

--- src/components/Products/CreateProducts.js
+++ src/components/Products/CreateProducts.js
@@ -7,23 +7,63 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import ProductManager from './ProductManager';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 export default function CreateProducts() {
   return (
-    <div>
-      <Box
-        sx={{
-          '& > :not(style)': { m: 9, width: '30ch' },
-          // width: '100%',
-          // maxWidth: 0,
-          // bgcolor: 'background.paper',
-        }}
-        autoComplete="on"
-      >
-        <h2>Crear Producto</h2>
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
 
-        <ProductManager />
-      </Box>
-    </div>
+      <div>
+        <Box
+          sx={{
+            '& > :not(style)': { m: 9, width: '30ch' },
+            // width: '100%',
+            // maxWidth: 0,
+            // bgTableCellor: 'background.paper',
+          }}
+          autoComplete="on"
+        >
+          <h2>Crear Producto</h2>
+
+          <ProductManager />
+        </Box>
+      </div>
+    </>
   );
 }
--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -15,7 +15,13 @@ function ProductItem({ onClose, open, title, description }) {
         justifyContent="center"
         style={{ minHeight: '100vh' }}
       > */}
-      <Box sx={{ mt: 10 }}>
+      <Box
+        sx={{ mt: 10 }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <h2>Product Item</h2>
         <h2>{title}</h2>
         <p>{description}</p>
 
