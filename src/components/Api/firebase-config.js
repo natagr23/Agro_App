09f7f4a0864097b35be5c800b75f553303390790
natagr23 <nata_garcia23@hotmail.com>
2023-04-08 18:20:52 -0500

    natalia: save firebase credentials in .env file

--- src/components/Api/firebase-config.js
+++ src/components/Api/firebase-config.js
@@ -10,9 +10,9 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCZM910LqZ-fhnETUNAl2UYyOTF1fCnPs8',
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: 'agro-app2-f9755',
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
