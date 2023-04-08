9c7fc062200cae47be32fd34f1dde894975ee4fb
natagr23 <nata_garcia23@hotmail.com>
2023-04-08 18:08:30 -0500

    natalia: update firebase app

--- src/components/Api/firebase-config.js
+++ src/components/Api/firebase-config.js
@@ -8,15 +8,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-//https://console.firebase.google.com/project/agroapp-ae569/overview
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyATqbxJ2psmEf_dGods0SuKAhyZOHLBHSU',
-  authDomain: 'agro-services-a96dc.firebaseapp.com',
-  projectId: 'agro-services-a96dc',
-  storageBucket: 'agro-services-a96dc.appspot.com',
-  messagingSenderId: '445686841502',
-  appId: '1:445686841502:web:945dec602a0ce2d77afb8f',
+  apiKey: 'AIzaSyCZM910LqZ-fhnETUNAl2UYyOTF1fCnPs8',
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: 'agro-app2-f9755',
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
