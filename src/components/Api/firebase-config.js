aeae37efc7e3173dbe10de23f26ff0c3efaee6c8
natagr23 <nata_garcia23@hotmail.com>
2023-04-08 19:42:48 -0500

    add project id to configuration file

--- src/components/Api/firebase-config.js
+++ src/components/Api/firebase-config.js
@@ -12,7 +12,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: 'agro-app2-f9755',
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
